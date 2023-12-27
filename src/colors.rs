dc8d982e1bd81abfda871d5d88448284ae2fa336
AnarchistHoneybun <thatonebipanda@gmail.com>
2023-12-28 03:20:37 +0530

    separated movement and coloring logic from main

---
+++ src/colors.rs
@@ -0,0 +1,73 @@
+use ratatui::prelude::Color;
+
+pub fn color_setter(value: u16) -> Color {
+    // check value and generate rgb values based on the value
+    if value == 0 {
+        return Color::Black;
+    }
+
+    //write a match statement that returns a Color::red based on the value
+
+    match value {
+        2 => Color::Rgb {
+            0: 238,
+            1: 228,
+            2: 218,
+        },
+        4 => Color::Rgb {
+            0: 237,
+            1: 224,
+            2: 200,
+        },
+        8 => Color::Rgb {
+            0: 242,
+            1: 177,
+            2: 121,
+        },
+        16 => Color::Rgb {
+            0: 245,
+            1: 149,
+            2: 99,
+        },
+        32 => Color::Rgb {
+            0: 246,
+            1: 124,
+            2: 96,
+        },
+        64 => Color::Rgb {
+            0: 246,
+            1: 94,
+            2: 59,
+        },
+        128 => Color::Rgb {
+            0: 237,
+            1: 207,
+            2: 114,
+        },
+        256 => Color::Rgb {
+            0: 237,
+            1: 204,
+            2: 97,
+        },
+        512 => Color::Rgb {
+            0: 237,
+            1: 200,
+            2: 80,
+        },
+        1024 => Color::Rgb {
+            0: 237,
+            1: 197,
+            2: 63,
+        },
+        2048 => Color::Rgb {
+            0: 237,
+            1: 194,
+            2: 46,
+        },
+        _ => Color::Rgb {
+            0: 237,
+            1: 194,
+            2: 46,
+        },
+    }
+}
